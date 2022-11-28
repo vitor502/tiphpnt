f20cca8d85d1664561ac961d71d5276a80f5687d
vitor502 <90069323+vitor502@users.noreply.github.com>
2022-11-28 19:26:04 -0300

    Add files via upload

---
+++ tiphpnt/index.php
@@ -0,0 +1,14 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Chuleta Quente Churrascaria</title>
+</head>
+<body>
+    
+</body>
+</html>
